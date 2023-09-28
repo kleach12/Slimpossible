d8641b3822ffa6cd861bb47adc8f462dca0b13c1
Kale Leach <kleach1212@gmail.com>
2023-09-27 21:25:38 -0700

    Added Styling for header

--- client/src/components/Header.js
+++ client/src/components/Header.js
@@ -1,14 +1,20 @@
-// This component is the header for the app it will be the top dashboard which will 
+// This component is the header for the app it will be the top dashboard which will
 // allow for user login and sign up and will also show the logo for the app
 
-import './Header.css'
+import "./Header.css";
 
 export default function Header() {
   return (
     <nav id="header">
       {/* logo */}
+      <button id="go-pro-btn" className="header-item">
+        {" "}
+        Go Pro{" "}
+      </button>
       {/* Sign up text */}
+      <p className="header-item"> Sign Up</p>
       {/* Login text */}
+      <p className="header-item"> Login</p>
     </nav>
   );
 }
