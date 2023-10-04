0aa99b57b5378791da8012b057501de1f5db6633
Kale Leach <kleach1212@gmail.com>
2023-10-03 20:44:39 -0700

    Created a weight input

--- client/src/components/WeightForm.js
+++ client/src/components/WeightForm.js
@@ -1,15 +1,44 @@
-import './WeightForm.css'
-import TextField from '@mui/material/TextField';
+import "./WeightForm.css";
+import { useState } from "react";
+import { AiOutlinePlus, AiOutlineMinus } from "react-icons/ai";
 
+export default function WeightForm() {
+  const [weight, setWeight] = useState(0);
 
-export default function WeightForm(){
-  return(
-    <div id = 'weight_form'>
+  const handleInputChange = (event) => {
+    setWeight(parseInt(event.target.value));
+  };
+
+  const decrementWeight = () => {
+    if(weight > 0){
+      setWeight(weight - 1);
+    }
+  };
+
+  const incrementWeight = () => {
+    setWeight(weight + 1);
+  }
+
+  console.log(weight);
+
+  return (
+    <div id="weight_form">
+      <h3 id="weight_form_title"> Enter today's weight</h3>
       <form>
-      <h3> Enter today's weight</h3>
-      <TextField id = 'weight_input' variant='filled'></TextField>
-      <input type='date'/>
+        <div>
+          <table>
+            <tbody>
+            <tr>
+              <td onClick={decrementWeight}><AiOutlineMinus/></td>
+              <td>
+                <input value={weight} onChange={handleInputChange}></input>
+              </td>
+              <td onClick={incrementWeight}> <AiOutlinePlus/></td>
+            </tr>
+            </tbody>
+          </table>
+        </div>
       </form>
     </div>
-  )
+  );
 }
